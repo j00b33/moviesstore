6de03b229eb502603858ecbddf52b54fb6ab8ebe
Jack Tran <jacktran@Mac.attlocal.net>
2025-01-31 17:50:03 -0500

    Chapter 6 completion, admin login: username:admin, password:1234

--- movies/views.py
+++ movies/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from .models import Movie
 movies = [
     {
         'id': 1, 'name': 'Inception', 'price': 12,
@@ -18,8 +19,21 @@ movies = [
     },
 ]
 def index(request):
+    search_term = request.GET.get('search')
+    if search_term:
+        movies = []
+        Movie.objects.filter(name__icontains=search_term)
+    else:
+        movies = Movie.objects.all()
     template_data = {}
     template_data['title'] = 'Movies'
     template_data['movies'] = movies
     return render(request, 'movies/index.html',
+                  {'template_data': template_data})
+def show(request, id):
+    movie =  Movie.objects.get(id=id)
+    template_data = {}
+    template_data['title'] = movie.name
+    template_data['movie'] = movie
+    return render(request, 'movies/show.html',
                   {'template_data': template_data})
