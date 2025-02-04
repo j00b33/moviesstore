f7169ffe218e774293e183bf7b3ee9655da5c713
Austin <azhang479@gatech.edu>
2025-02-03 19:35:13 -0500

    fixed missing logout/empty url pages

--- home/urls.py
+++ home/urls.py
@@ -3,4 +3,5 @@ from . import views
 urlpatterns = [
     path('index/', views.index, name='home.index'),
     path('about/', views.about, name='home.about'),
+    path('', views.about, name='home.index'),
 ]
