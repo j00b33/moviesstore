02a8d13e0bc1bb8ce19cc6803b21b13d86d092e5
Austin <azhang479@gatech.edu>
2025-02-11 15:37:58 -0500

    fixed cart

--- cart/templatetags/cart_filters.py
+++ cart/templatetags/cart_filters.py
@@ -2,4 +2,4 @@ from django import template
 register = template.Library()
 @register.filter(name='get_quantity')
 def get_cart_quantity(cart, movie_id):
-    return cart[str(movie_id)]
+    return cart[movie_id]
