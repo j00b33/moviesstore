3dfce820cd05b1a7fbffa0b33a83cbf41657ba6d
CNW-beep45 <duy.ddo3@gmail.com>
2025-02-11 17:52:21 -0500

    Password progress

--- accounts/views.py
+++ accounts/views.py
@@ -4,6 +4,9 @@ from .forms import CustomUserCreationForm, CustomErrorList
 from django.shortcuts import redirect
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.models import User
+from django.contrib.auth import get_user_model, update_session_auth_hash
+from django.contrib.auth.forms import SetPasswordForm
+from django.shortcuts import render, redirect
 @login_required
 def logout(request):
     auth_logout(request)
@@ -45,6 +48,29 @@ def signup(request):
             template_data['form'] = form
             return render(request, 'accounts/signup.html',
                 {'template_data': template_data})
+
+def passwordReset(request):
+    template_data = {}
+    template_data['title'] = 'Password Reset'
+
+    if request.method == 'GET':
+        return render(request, 'accounts/password_reset.html', {'template_data': template_data})
+
+    elif request.method == 'POST':
+        username = request.POST.get('username')
+        new_password = request.POST.get('new_password')
+
+        try:
+            user = get_user_model().objects.get(username=username)
+        except get_user_model().DoesNotExist:
+            template_data['error'] = '* The username does not exist.'
+            return render(request, 'accounts/password_reset.html', {'template_data': template_data})
+
+        # Set the new password
+        user.set_password(new_password)
+        user.save()
+
+
 @login_required
 def orders(request):
     template_data = {}
@@ -52,4 +78,6 @@ def orders(request):
     template_data['orders'] = request.user.order_set.all()
     return render(request, 'accounts/orders.html',
         {'template_data': template_data})
+
+
 # Create your views here.
